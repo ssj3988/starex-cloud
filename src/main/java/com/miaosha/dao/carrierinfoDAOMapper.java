14c46921a73a52e59f55c1adc719278f1d83d7fc
sunshengjun3 <sunshengjun3@jd.com>
2020-08-05 16:49:53 +0800

    修改文件

--- src/main/java/com/miaosha/dao/carrierinfoDAOMapper.java
+++ src/main/java/com/miaosha/dao/carrierinfoDAOMapper.java
@@ -3,8 +3,9 @@ package com.miaosha.dao;
 import com.miaosha.dataobject.carrierinfoDAO;
 
 public interface carrierinfoDAOMapper {
+     //删除方法
     int deleteByPrimaryKey(Long id);
-
+    //插入方法
     int insert(carrierinfoDAO record);
 
     int insertSelective(carrierinfoDAO record);
